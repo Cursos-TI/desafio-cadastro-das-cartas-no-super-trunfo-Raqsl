a1d73e77cb78c2eb64be650e1316c93260be7274
Raqsl <raqslsantos@gmail.com>
2025-08-13 00:22:51 +0000

    Tudo digitado. Em teste

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,7 +3,8 @@
 
 
 int main() {
-    printf("Desafio Cartas SuperTrunfo!");
+    printf("Desafio Cartas SuperTrunfo!\n");
+    printf("Carta 01 \n");
 
     char Estado[20];
     char Código [20];
@@ -14,13 +15,13 @@ int main() {
     int PontosTurísticos;
 
     printf("Estado:");
-    scanf("%c", & Estado);
+    scanf("%s", Estado);
 
     printf("Código:");
-    scanf("%c", &Código);
+    scanf("%s", Código);
 
     printf("Cidade:");
-    Scanf("%c", Cidade);
+    scanf("%s", Cidade);
 
     printf("População:");
     scanf("%d", &População);
@@ -35,6 +36,57 @@ int main() {
     scanf("%d", &PontosTurísticos);
 
 
+     print("Carta 02 \n");
+
+    char Estado2[20];
+    char Código2 [20];
+    char Cidade2 [20];
+    int População2;
+    float Área2;
+    float PIB2;
+    int PontosTurísticos2;
+
+    printf("Estado:");
+    scanf("%s", Estado2);
+
+    printf("Código:");
+    scanf("%s", Código2);
+
+    printf("Cidade:");
+    scanf("%s", Cidade2);
+
+    printf("População:");
+    scanf("%d", &População2);
+
+    printf("Área em Km²:");
+    scanf("%f", &Área2);
+
+    printf("PIB: ");
+    scanf("%f", &PIB2);
+    
+    printf("Pontos Turísticos:");
+    scanf("%d", &PontosTurísticos2);
+
+
+    printf("\n--- Dados da Carta 1 ---\n");
+
+    printf("Estado: %s\n", Estado);
+    printf("Código: %s\n", Código);
+    printf("Cidade: %s\n", Cidade);
+    printf("População: %d\n", População);
+    printf("Área: %.2f Km²\n", Área);
+    printf("PIB: %.2f\n", PIB);
+    printf("Pontos Turísticos: %d\n", PontosTurísticos); 
+
+    printf("\n--- Dados da Carta 2 ---\n");
+
+    printf("Estado: %s\n", Estado2);
+    printf("Código: %s\n", Código2);
+    printf("Cidade: %s\n", Cidade2);
+    printf("População: %d\n", População2);
+    printf("Área: %.2f Km²\n", Área2);
+    printf("PIB: %.2f\n", PIB2);
+    printf("Pontos Turísticos: %d\n", PontosTurísticos2);
 
 
 }
